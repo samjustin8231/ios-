8ab3fb5f3053f3633dc96b77b7d6d1dd1408c4d0
yajun.sun <yajun.sun@cootek.cn>
2017-09-05 18:22:53 +0800

    add project 多视图1 about two pages

---
+++ 多视图1/多视图1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  多视图1
+//
+//  Created by sam on 2017/9/5.
+//  Copyright © 2017年 sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 多视图1/多视图1/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  多视图1
+//
+//  Created by sam on 2017/9/5.
+//  Copyright © 2017年 sam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)prefersStatusBarHidden{
+    return YES;
+}
+
+@end
