7c75306fe1863181bc252460a877deeb45c78b04
yajun.sun <yajun.sun@cootek.cn>
2017-09-11 14:35:34 +0800

    add login pager

---
+++ 聊天记录/聊天记录/NYLoginViewController.m
@@ -0,0 +1,50 @@
+//
+//  NYLoginViewController.m
+//  聊天记录
+//
+//  Created by sam on 2017/9/11.
+//  Copyright © 2017年 sam. All rights reserved.
+//
+
+#import "NYLoginViewController.h"
+#import "NYGroupTableViewController.h"
+
+@interface NYLoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *tfNumber;
+@property (weak, nonatomic) IBOutlet UITextField *tfCode;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnLogin;
+
+- (IBAction)getCode;
+
+- (IBAction)login;
+
+@end
+
+@implementation NYLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)getCode {
+}
+
+- (IBAction)login {
+    NSLog(@"login");
+    
+    //跳转
+    UIStoryboard *storboard = self.storyboard;
+    NYGroupTableViewController *viewController = [storboard instantiateViewControllerWithIdentifier:@"groupPager"];
+    
+    //viewController.statusTitle.titleView
+    [self presentViewController:viewController animated:YES completion:nil];
+}
+@end
