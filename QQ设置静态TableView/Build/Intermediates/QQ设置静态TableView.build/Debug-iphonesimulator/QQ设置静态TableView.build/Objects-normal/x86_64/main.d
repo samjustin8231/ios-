771d0067791d657e8dd296176249fa9a18082bea
yajun.sun <yajun.sun@cootek.cn>
2017-09-25 17:42:56 +0800

    other files for qq static uitableview cell;

---
+++ QQ设置静态TableView/Build/Intermediates/QQ设置静态TableView.build/Debug-iphonesimulator/QQ设置静态TableView.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/sam/Workspaces/ios_workspace/ios-study/QQ设置静态TableView/QQ设置静态TableView/main.m \
+  /Users/sam/Workspaces/ios_workspace/ios-study/QQ设置静态TableView/QQ设置静态TableView/AppDelegate.h
